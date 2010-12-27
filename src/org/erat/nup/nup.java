09c1b18c4015bc630983d7097b71fbc61cadb675
Daniel Erat <dan@erat.org>
2010-12-27 12:34:43 -0800

    android skeleton files

---
+++ src/org/erat/nup/nup.java
@@ -0,0 +1,15 @@
+package org.erat.nup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class nup extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
