a2743def0b9147f2d9bad7b84d8c428c6b7421ad
Daniel Erat <dan@erat.org>
2010-12-30 16:11:21 -0800

    send search requests (well, just one request right now) and read json

--- src/org/erat/nup/NupActivity.java
+++ src/org/erat/nup/NupActivity.java
@@ -5,12 +5,22 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class NupActivity extends Activity implements NupServiceObserver {
     private NupService mService;
@@ -57,7 +67,56 @@ public class NupActivity extends Activity implements NupServiceObserver {
         }
     };
 
-    public void onSearchButtonClicked(View view) {
+    class SendSearchRequestTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            final String userAgent = "whatever";
+            AndroidHttpClient client = AndroidHttpClient.newInstance(userAgent, NupActivity.this);
+            HttpResponse response;
+            try {
+                response = client.execute(new HttpGet(urls[0]));
+            } catch (IOException err) {
+                Log.e(this.toString(), "query failed");
+                return "";
+            }
+            Log.i(this.toString(), "got response from server");
+
+            // Yay.
+            StringBuilder sb = new StringBuilder();
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException err) {
+                return "";
+            }
+
+            try {
+                JSONArray songs;
+                songs = (JSONArray) new JSONTokener(sb.toString()).nextValue();
+                for (int i = 0; i < songs.length(); ++i) {
+                    JSONObject song = songs.getJSONObject(i);
+                    Log.i(this.toString(), song.getString("artist") + " - " + song.getString("title"));
+                }
+            } catch (org.json.JSONException err) {
+                Log.e(this.toString(), "unable to parse json");
+                return "";
+            }
+
+            return "foo";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (mService != null) {
+            }
+        }
+    }
+
+    public void onSearchButtonClicked(View view) throws IOException {
+        new SendSearchRequestTask().execute("http://10.0.0.5:8080/query?artist=Bola");
     }
 
     public void onPauseButtonClicked(View view) {
