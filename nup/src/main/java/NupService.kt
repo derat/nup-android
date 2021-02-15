derat/nup-android

commit b63e54f32596676b5b6777d0631416ad5483f4b2

Get Kotlin working, I think. Also fix minor style stuff.

nup/src/main/java/NupService.kt
@@ -49,7 +49,7 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
         fun onSongCoverLoad(song: Song?)
 
         // Invoked when the current set of songs to be played changes.
-        fun onPlaylistChange(songs: List<Song>?)
+        fun onPlaylistChange(songs: List<Song>)
     }
 
     // Listener for the song database's aggregate data being updated.
@@ -112,7 +112,7 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
     private var shouldDownloadAll = false
 
     // Current playlist.
-    private val songs: MutableList<Song> = ArrayList()
+    private val songs = ArrayList<Song>()
 
     // Index of the song in |mSongs| that's being played.
     var currentSongIndex = -1
@@ -786,11 +786,10 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
     // Plays the first one, if no song is already playing or if we were previously at the end of the
     // playlist and we've appended to it. Returns true if we started playing.
     private fun insertSongs(insSongs: List<Song>, index: Int): Boolean {
-        var insSongs = insSongs
         if (index < 0 || index > songs.size) {
             Log.e(
                     TAG,
-                    "ignoring request to insert " + insSongs.size + " song(s) at index " + index)
+                    "ignoring request to insert ${insSongs.size} song(s) at index $index")
             return false
         }
 
@@ -799,32 +798,32 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
         val newSongs = ArrayList<Song>()
 
         // Use our own version of each song if we have it already.
-        val tmpSongs = ArrayList<Song>()
+        val resSongs = ArrayList<Song>()
         for (song in insSongs) {
             val ourSong = songIdToSong[song.id]
             if (ourSong != null) {
-                tmpSongs.add(ourSong)
+                resSongs.add(ourSong)
             } else {
-                tmpSongs.add(song)
+                resSongs.add(song)
                 newSongs.add(song)
             }
         }
-        insSongs = tmpSongs
-        songs.addAll(index, insSongs)
+
+        songs.addAll(index, resSongs)
         if (currentSongIndex >= 0 && index <= currentSongIndex) {
-            currentSongIndex += insSongs.size
+            currentSongIndex += resSongs.size
         }
         if (downloadIndex >= 0 && index <= downloadIndex) {
-            downloadIndex += insSongs.size
+            downloadIndex += resSongs.size
         }
-        if (songListener != null) songListener!!.onPlaylistChange(songs)
+        songListener?.onPlaylistChange(songs)
         mediaSessionManager!!.updatePlaylist(songs)
         for (song in newSongs) {
             songIdToSong[song.id] = song
             val entry = cache!!.getEntry(song.id)
             if (entry != null) {
                 song.updateBytes(entry)
-                if (songListener != null) songListener!!.onSongFileSizeChange(song)
+                songListener?.onSongFileSizeChange(song)
             }
         }
         var played = false
@@ -878,20 +877,18 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
     }
 
     // Try to download the next not-yet-downloaded song in the playlist.
-    private fun maybeDownloadAnotherSong(index: Int) {
-        var index = index
+    private fun maybeDownloadAnotherSong(startIndex: Int) {
         if (downloadSongId != -1L) {
             Log.e(
                     TAG,
-                    "aborting prefetch since download of song "
-                            + downloadSongId
-                            + " is still in progress")
+                    "aborting prefetch since download of song $downloadSongId is still in progress")
             return
         }
         val songsToPreload = Integer.valueOf(
                 prefs!!.getString(
                         NupPreferences.SONGS_TO_PRELOAD,
-                        NupPreferences.SONGS_TO_PRELOAD_DEFAULT))
+                        NupPreferences.SONGS_TO_PRELOAD_DEFAULT)!!)
+        var index = startIndex
         while (index < songs.size
                 && (shouldDownloadAll || index - currentSongIndex <= songsToPreload)) {
             val song = songs[index]
@@ -903,12 +900,10 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
                 index++
                 continue
             }
-            entry = cache!!.downloadSong(song)
             downloadSongId = song.id
             downloadIndex = index
             cache!!.pinSongId(song.id)
             fetchCoverForSongIfMissing(song)
-            return
             index++
         }
     }

nup/src/main/java/BrowseActivityBase.kt
@@ -29,7 +29,7 @@ open class BrowseActivityBase : ListActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == RESULT_OK) {
             setResult(RESULT_OK)
             finish()

nup/src/main/java/BrowseAlbumsActivity.kt
@@ -53,8 +53,7 @@ class BrowseAlbumsActivity : BrowseActivityBase(), SongDatabaseUpdateListener {
     }
 
     override fun onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
-        val row = rows[position] ?: return
-
+        val row = rows[position]
         // TODO: Right now, we show the full album by default instead of limiting it to songs by
         // |mArtist|. Decide if this makes sense.
         startBrowseSongsActivity(null, row.key.album, row.key.albumId, -1.0)
@@ -63,8 +62,7 @@ class BrowseAlbumsActivity : BrowseActivityBase(), SongDatabaseUpdateListener {
     override fun onCreateContextMenu(
             menu: ContextMenu, view: View, menuInfo: ContextMenuInfo) {
         val pos = (menuInfo as AdapterContextMenuInfo).position
-        val row = rows[pos]
-        if (row != null) menu.setHeaderTitle(row.key.album)
+        menu.setHeaderTitle(rows[pos].key.album)
         if (artist != null) {
             val msg = getString(R.string.browse_songs_by_artist_fmt, artist)
             menu.add(0, MENU_ITEM_BROWSE_SONGS_BY_ARTIST, 0, msg)
@@ -75,7 +73,7 @@ class BrowseAlbumsActivity : BrowseActivityBase(), SongDatabaseUpdateListener {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         val info = item.menuInfo as AdapterContextMenuInfo
-        val row = rows[info.position] ?: return false
+        val row = rows[info.position]
         return when (item.itemId) {
             MENU_ITEM_BROWSE_SONGS_BY_ARTIST -> {
                 startBrowseSongsActivity(artist, row.key.album, row.key.albumId, -1.0)

nup/src/main/java/BrowseArtistsActivity.kt
@@ -46,15 +46,13 @@ class BrowseArtistsActivity : BrowseActivityBase(), SongDatabaseUpdateListener {
     }
 
     override fun onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
-        val row = rows[position] ?: return
-        startBrowseAlbumsActivity(row.key.artist)
+        startBrowseAlbumsActivity(rows[position].key.artist)
     }
 
     override fun onCreateContextMenu(
             menu: ContextMenu, view: View, menuInfo: ContextMenuInfo) {
         val pos = (menuInfo as AdapterContextMenuInfo).position
-        val row = rows[pos]
-        if (row != null) menu.setHeaderTitle(row.key.artist)
+        menu.setHeaderTitle(rows[pos].key.artist)
         menu.add(0, MENU_ITEM_BROWSE_SONGS_WITH_RATING, 0, R.string.browse_songs_four_stars)
         menu.add(0, MENU_ITEM_BROWSE_SONGS, 0, R.string.browse_songs)
         menu.add(0, MENU_ITEM_BROWSE_ALBUMS, 0, R.string.browse_albums)
@@ -62,7 +60,7 @@ class BrowseArtistsActivity : BrowseActivityBase(), SongDatabaseUpdateListener {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         val info = item.menuInfo as AdapterContextMenuInfo
-        val row = rows[info.position] ?: return false
+        val row = rows[info.position]
         return when (item.itemId) {
             MENU_ITEM_BROWSE_SONGS_WITH_RATING -> {
                 startBrowseSongsActivity(row.key.artist, 0.75)

nup/src/main/java/BrowseSongsActivity.kt
@@ -135,7 +135,7 @@ class BrowseSongsActivity : Activity(), OnItemClickListener {
             menu: ContextMenu, view: View, menuInfo: ContextMenuInfo) {
         if (view.id == R.id.songs) {
             val info = menuInfo as AdapterContextMenuInfo
-            val song = songs[info.position] ?: return
+            val song = songs[info.position]
             menu.setHeaderTitle(song.title)
             menu.add(0, MENU_ITEM_PLAY, 0, R.string.play)
             menu.add(0, MENU_ITEM_INSERT, 0, R.string.insert)
@@ -146,7 +146,7 @@ class BrowseSongsActivity : Activity(), OnItemClickListener {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         val info = item.menuInfo as AdapterContextMenuInfo
-        val song = songs[info.position] ?: return false
+        val song = songs[info.position]
         return when (item.itemId) {
             MENU_ITEM_PLAY -> {
                 service!!.addSongToPlaylist(song, true)
@@ -170,7 +170,7 @@ class BrowseSongsActivity : Activity(), OnItemClickListener {
 
     // Implements AdapterView.OnItemClickListener.
     override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-        val song = songs[position] ?: return
+        val song = songs[position]
         service!!.appendSongToPlaylist(song)
         Toast.makeText(this, getString(R.string.appended_song_fmt, song.title), Toast.LENGTH_SHORT)
                 .show()
@@ -185,21 +185,21 @@ class BrowseSongsActivity : Activity(), OnItemClickListener {
         if (id == DIALOG_SONG_DETAILS) prepareDialog(dialog, args)
     }
 
-    fun onAppendButtonClicked(view: View?) {
+    fun onAppendButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
         if (songs.isEmpty()) return
         service!!.appendSongsToPlaylist(songs)
         setResult(RESULT_OK)
         finish()
     }
 
-    fun onInsertButtonClicked(view: View?) {
+    fun onInsertButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
         if (songs.isEmpty()) return
         service!!.addSongsToPlaylist(songs, false)
         setResult(RESULT_OK)
         finish()
     }
 
-    fun onReplaceButtonClicked(view: View?) {
+    fun onReplaceButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
         if (songs.isEmpty()) return
         service!!.clearPlaylist()
         service!!.appendSongsToPlaylist(songs)

nup/src/main/java/CrashLogger.kt
@@ -32,7 +32,7 @@ class CrashLogger private constructor(private val dir: File) : Thread.UncaughtEx
         } catch (e: IOException) {
             Log.e(TAG, "IO error: $e")
         }
-        defaultHandler.uncaughtException(thread, error)
+        defaultHandler?.uncaughtException(thread, error)
     }
 
     companion object {

nup/src/main/java/DatabaseUpdater.kt
@@ -30,6 +30,6 @@ internal class DatabaseUpdater(private val opener: DatabaseOpener) : Runnable {
     }
 
     fun postUpdate(sql: String?, values: Array<Any>?) {
-        handler!!.post { opener.getDb()!!.execSQL(sql, values) }
+        handler!!.post { opener.getDb().execSQL(sql, values) }
     }
 }

nup/src/main/java/NupActivity.kt
@@ -46,7 +46,7 @@ internal class NupActivity : Activity(), SongListener {
     private var lastSongPositionSec = -1
 
     // Songs in the current playlist.
-    private var songs: List<Song>? = ArrayList()
+    private var songs: List<Song> = ArrayList<Song>()
 
     // Position in |songs| of the song that we're currently displaying.
     private var currentSongIndex = -1
@@ -130,7 +130,7 @@ internal class NupActivity : Activity(), SongListener {
             }
 
             // TODO: Go to prefs page if server and account are unset.
-            if (songs!!.isEmpty()) {
+            if (songs.isEmpty()) {
                 startActivity(Intent(this@NupActivity, BrowseTopActivity::class.java))
             }
         }
@@ -141,19 +141,19 @@ internal class NupActivity : Activity(), SongListener {
         }
     }
 
-    fun onPauseButtonClicked(view: View?) {
+    fun onPauseButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
         service!!.togglePause()
     }
 
-    fun onPrevButtonClicked(view: View?) {
+    fun onPrevButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
         if (currentSongIndex <= 0) return
         service!!.stopPlaying()
         updateCurrentSongIndex(currentSongIndex - 1)
         schedulePlaySongTask(SONG_CHANGE_DELAY_MS)
     }
 
-    fun onNextButtonClicked(view: View?) {
-        if (currentSongIndex >= songs!!.size - 1) return
+    fun onNextButtonClicked(@Suppress("UNUSED_PARAMETER") view: View?) {
+        if (currentSongIndex >= songs.size - 1) return
         service!!.stopPlaying()
         updateCurrentSongIndex(currentSongIndex + 1)
         schedulePlaySongTask(SONG_CHANGE_DELAY_MS)
@@ -180,8 +180,8 @@ internal class NupActivity : Activity(), SongListener {
     }
 
     // Implements NupService.SongListener.
-    override fun onPauseStateChange(isPaused: Boolean) {
-        runOnUiThread { pauseButton!!.text = getString(if (isPaused) R.string.play else R.string.pause) }
+    override fun onPauseStateChange(paused: Boolean) {
+        runOnUiThread { pauseButton!!.text = getString(if (paused) R.string.play else R.string.pause) }
     }
 
     // Implements NupService.SongListener.
@@ -193,10 +193,10 @@ internal class NupActivity : Activity(), SongListener {
     }
 
     // Implements NupService.SongListener.
-    override fun onPlaylistChange(newSongs: List<Song>?) {
+    override fun onPlaylistChange(songs: List<Song>) {
         runOnUiThread {
-            songs = newSongs
-            findViewById<View>(R.id.playlist_heading).visibility = if (songs!!.isEmpty()) View.INVISIBLE else View.VISIBLE
+            this.songs = songs
+            findViewById<View>(R.id.playlist_heading).visibility = if (this.songs.isEmpty()) View.INVISIBLE else View.VISIBLE
             updateCurrentSongIndex(service!!.currentSongIndex)
         }
     }
@@ -298,7 +298,7 @@ internal class NupActivity : Activity(), SongListener {
     // Adapts our information about the current playlist and song for the song list view.
     private inner class SongListAdapter : BaseAdapter() {
         override fun getCount(): Int {
-            return songs!!.size
+            return songs.size
         }
 
         override fun getItem(position: Int): Any {
@@ -309,9 +309,8 @@ internal class NupActivity : Activity(), SongListener {
             return position.toLong()
         }
 
-        override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-            val view: View
-            view = if (convertView != null) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = if (convertView != null) {
                 convertView
             } else {
                 val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -320,15 +319,14 @@ internal class NupActivity : Activity(), SongListener {
             val artistView = view.findViewById<View>(R.id.artist) as TextView
             val titleView = view.findViewById<View>(R.id.title) as TextView
             val percentView = view.findViewById<View>(R.id.percent) as TextView
-            val song = songs!![position]
+            val song = songs[position]
             artistView.text = song.artist
             titleView.text = song.title
             if (song.totalBytes > 0) {
                 if (song.availableBytes == song.totalBytes) percentView.text = "\u2713" // CHECK MARK from Dingbats
-                else percentView.setText((Math.round(100.0
+                else percentView.setText(Math.round(100.0
                         * song.availableBytes
-                        / song.totalBytes) as Int)
-                .toString() + "%")
+                        / song.totalBytes).toInt().toString() + "%")
                 percentView.visibility = View.VISIBLE
             } else {
                 percentView.visibility = View.GONE
@@ -355,7 +353,7 @@ internal class NupActivity : Activity(), SongListener {
             menu: ContextMenu, view: View, menuInfo: ContextMenuInfo) {
         if (view.id == R.id.playlist) {
             val info = menuInfo as AdapterContextMenuInfo
-            val song = songs!![info.position]
+            val song = songs[info.position]
             menu.setHeaderTitle(song.title)
             menu.add(0, MENU_ITEM_PLAY, 0, R.string.play)
             menu.add(0, MENU_ITEM_REMOVE_FROM_LIST, 0, R.string.remove_from_list)
@@ -366,7 +364,7 @@ internal class NupActivity : Activity(), SongListener {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         val info = item.menuInfo as AdapterContextMenuInfo
-        val song = songs!![info.position]
+        val song = songs[info.position]
         return when (item.itemId) {
             MENU_ITEM_PLAY -> {
                 updateCurrentSongIndex(info.position)
@@ -378,11 +376,11 @@ internal class NupActivity : Activity(), SongListener {
                 true
             }
             MENU_ITEM_TRUNCATE_LIST -> {
-                service!!.removeRangeFromPlaylist(info.position, songs!!.size - 1)
+                service!!.removeRangeFromPlaylist(info.position, songs.size - 1)
                 true
             }
             MENU_ITEM_SONG_DETAILS -> {
-                if (song != null) showDialog(DIALOG_SONG_DETAILS, createBundle(song))
+                showDialog(DIALOG_SONG_DETAILS, createBundle(song))
                 true
             }
             else -> false
@@ -399,8 +397,8 @@ internal class NupActivity : Activity(), SongListener {
     }
 
     private val currentSong: Song?
-        private get() = if (currentSongIndex >= 0 && currentSongIndex < songs!!.size) {
-            songs!![currentSongIndex]
+        get() = if (currentSongIndex >= 0 && currentSongIndex < songs.size) {
+            songs[currentSongIndex]
         } else null
 
     private fun updateCurrentSongIndex(index: Int) {
@@ -408,8 +406,8 @@ internal class NupActivity : Activity(), SongListener {
         updateSongDisplay(currentSong)
         songListAdapter.notifyDataSetChanged()
         prevButton!!.isEnabled = currentSongIndex > 0
-        nextButton!!.isEnabled = !songs!!.isEmpty() && currentSongIndex < songs!!.size - 1
-        pauseButton!!.isEnabled = !songs!!.isEmpty()
+        nextButton!!.isEnabled = !songs.isEmpty() && currentSongIndex < songs.size - 1
+        pauseButton!!.isEnabled = !songs.isEmpty()
     }
 
     private fun schedulePlaySongTask(delayMs: Int) {

commit 0612c18df242aaf6c21846b4b1a304c58d6f1360

Restore missing download call when preloading. Fixes #4.
b63e54f3 accidentally removed a call to tell the cache to
download the song.

nup/src/main/java/NupService.kt
@@ -900,6 +900,7 @@ internal class NupService : Service(), Player.Listener, FileCache.Listener, Song
                 index++
                 continue
             }
+            entry = cache!!.downloadSong(song)
             downloadSongId = song.id
             downloadIndex = index
             cache!!.pinSongId(song.id)