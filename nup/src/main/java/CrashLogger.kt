9b14d5384fb6317748c659f12dd515e52bcd1fdf
Daniel Erat <dan@erat.org>
2021-02-19 09:30:17 -0400

    Clean up CrashLogger.

--- nup/src/main/java/CrashLogger.kt
+++ nup/src/main/java/CrashLogger.kt
@@ -9,43 +9,49 @@ import android.util.Log
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
-import java.io.PrintWriter
 import java.text.SimpleDateFormat
 import java.util.Date
 
+/** Writes uncaught exceptions to text files and rethrows them. */
 class CrashLogger private constructor(private val dir: File) : Thread.UncaughtExceptionHandler {
     private val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+    private val dateFormat = SimpleDateFormat("yyyyMMdd-HHmmss")
+
     override fun uncaughtException(thread: Thread, error: Throwable) {
         try {
             dir.mkdirs()
-            val format = SimpleDateFormat("yyyyMMdd-HHmmss")
-            val file = File(dir, format.format(Date()) + ".txt")
-            Log.d(TAG, "creating crash file ${file.absolutePath}")
+            val file = File(dir, dateFormat.format(Date()) + ".txt")
+            Log.d(TAG, "Creating crash file ${file.absolutePath}")
             file.createNewFile()
-            val writer = PrintWriter(file)
-            error.printStackTrace(writer)
-            val cause = error.cause
-            if (cause != null) {
-                writer.print("\n\nCaused by:\n")
-                cause.printStackTrace(writer)
+
+            file.printWriter().use { out ->
+                error.printStackTrace(out)
+                if (error.cause != null) {
+                    out.print("\n\nCaused by:\n")
+                    error.cause?.printStackTrace(out)
+                }
             }
-            writer.close()
         } catch (e: FileNotFoundException) {
-            Log.e(TAG, "file not found: $e")
+            Log.e(TAG, "File not found: $e")
         } catch (e: IOException) {
             Log.e(TAG, "IO error: $e")
         }
+
         defaultHandler?.uncaughtException(thread, error)
     }
 
     companion object {
         private const val TAG = "CrashLogger"
         private var singleton: CrashLogger? = null
+
+        /** Register the crash reporter. */
         fun register(dir: File) {
             if (singleton != null) return
             singleton = CrashLogger(dir)
             Thread.setDefaultUncaughtExceptionHandler(singleton)
         }
+
+        /** Unregister the crash reporter. */
         fun unregister() {
             if (singleton == null) return
             Thread.setDefaultUncaughtExceptionHandler(null)
