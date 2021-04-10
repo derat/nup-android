derat/nup-android

commit 5440343aade24a9ba40ce7514bd5a67c215f57e3

Send position update at end of song. Fixes #20.
Make Player call onPlaybackPositionChange() one final time
after playback finishes. Otherwise, NupActivity sometimes
shows the final song as not being completely played.

nup/src/main/java/NupActivity.kt
@@ -177,9 +177,12 @@ class NupActivity : AppCompatActivity(), NupService.SongListener {
                 if (song != curSong) return@task
                 val sec = positionMs / 1000
                 if (sec == lastPosSec) return@task
-                // MediaPlayer appears to get confused sometimes and report things like 0:01.
-                // TODO: Is this still the case?
-                val durationSec = Math.max(durationMs / 1000, song.lengthSec)
+
+                // If we've already downloaded the whole song, use the duration computed by
+                // MediaPlayer in case it's different from the length in the database.
+                val durationSec =
+                    if (song.availableBytes == song.totalBytes) durationMs / 1000
+                    else song.lengthSec
                 timeLabel.text = formatDurationProgress(sec, durationSec)
                 lastPosSec = sec
             }

nup/src/main/java/Player.kt
@@ -371,6 +371,14 @@ class Player(
                         )
                     }
                 } else {
+                    // Call onPlaybackPositionChange() one final time to say that we're at the end.
+                    // Otherwise, we may show a not-quite-complete position at the end of the final
+                    // track: https://github.com/derat/nup-android/issues/20
+                    val file = filePlayer.file
+                    val durationMs = filePlayer.mediaPlayer.duration
+                    listenerExecutor.execute {
+                        listener.onPlaybackPositionChange(file, durationMs, durationMs)
+                    }
                     resetCurrent()
                     listenerExecutor.execute { listener.onPlaybackComplete() }
                 }

commit c56fa5749ebe52e4a21876675aa5b78c9eceb340

Display correct song duration after connecting to service.
Fix an issue introduced by 5440343aade where NupActivity
would initially display a song duration of 0:00 after
connecting to an already-started service.

nup/src/main/java/NupActivity.kt
@@ -133,7 +133,7 @@ class NupActivity : AppCompatActivity(), NupService.SongListener {
             onPauseStateChange(service.paused)
             val song = curSong
             if (song != null && song == service.curSong) {
-                onSongPositionChange(song, service.lastPosMs, 0)
+                onSongPositionChange(song, service.lastPosMs, -1)
                 playlistView.smoothScrollToPosition(curSongIndex)
             }
 
@@ -180,8 +180,9 @@ class NupActivity : AppCompatActivity(), NupService.SongListener {
 
                 // If we've already downloaded the whole song, use the duration computed by
                 // MediaPlayer in case it's different from the length in the database.
+                val downloaded = song.availableBytes == song.totalBytes
                 val durationSec =
-                    if (song.availableBytes == song.totalBytes) durationMs / 1000
+                    if (downloaded && durationMs >= 0) durationMs / 1000
                     else song.lengthSec
                 timeLabel.text = formatDurationProgress(sec, durationSec)
                 lastPosSec = sec