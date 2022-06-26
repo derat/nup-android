derat/nup-android

commit 85b9593b1c89869b2f405f2a2cd8e025d5814059

Make MediaBrowserHelper show search presets for #29.
Update SongDatabase to sync search presets from the server's
/presets endpoint, and update MediaBrowserHelper to include
a top-level "Presets" tab.

nup/src/main/java/SearchResultsActivity.kt
@@ -21,15 +21,9 @@ import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.Serializable
-import java.net.URLEncoder
-import java.util.Date
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import org.erat.nup.NupActivity.Companion.service
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import org.json.JSONTokener
 
 /** Displays a list of songs from a search result. */
 class SearchResultsActivity : AppCompatActivity() {
@@ -155,9 +149,6 @@ class SearchResultsActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean =
         handleCommonOptionsItemSelected(item)
 
-    /** Thrown if an error is encountered while searching. */
-    class SearchException(reason: String) : Exception(reason)
-
     /** Perform the search specified in [intent] using [SongDatabase]. */
     private suspend fun doLocalSearch(): List<Song> =
         service.songDb.query(
@@ -184,58 +175,20 @@ class SearchResultsActivity : AppCompatActivity() {
 
     /** Perform the search specified in [intent] over the network. */
     @Throws(SearchException::class)
-    private suspend fun doNetworkSearch(): List<Song> {
-        var params = ""
-        val add = fun(k: String, v: String?) {
-            if (v.isNullOrEmpty()) return
-            if (!params.isEmpty()) params += "&"
-            params += "$k=${URLEncoder.encode(v, "utf-8")}"
-        }
-
-        add("artist", intent.getStringExtra(BUNDLE_ARTIST))
-        add("title", intent.getStringExtra(BUNDLE_TITLE))
-        add("album", intent.getStringExtra(BUNDLE_ALBUM))
-        add("shuffle", if (intent.getBooleanExtra(BUNDLE_SHUFFLE, false)) "1" else "")
-        add("keywords", intent.getStringExtra(BUNDLE_KEYWORDS))
-        add("tags", intent.getStringExtra(BUNDLE_TAGS))
-
-        val rating = intent.getDoubleExtra(BUNDLE_MIN_RATING, -1.0)
-        add("minRating", if (rating >= 0) "%.2f".format(rating) else "")
-
-        val maxPlays = intent.getIntExtra(BUNDLE_MAX_PLAYS, -1)
-        add("maxPlays", if (maxPlays >= 0) maxPlays.toString() else "")
-
-        // We get numbers of seconds before the current time from SearchFormActivity, but the server
-        // wants timestamps as seconds since the epoch.
-        val now = Date().getTime() / 1000
-        val firstPlayed = intent.getIntExtra(BUNDLE_FIRST_PLAYED, 0)
-        if (firstPlayed > 0) add("minFirstPlayed", (now - firstPlayed).toString())
-        val lastPlayed = intent.getIntExtra(BUNDLE_LAST_PLAYED, 0)
-        if (lastPlayed > 0) add("maxLastPlayed", (now - lastPlayed).toString())
-
-        val (response, error) = service.downloader.downloadString("/query?$params")
-        response ?: throw SearchException(error!!)
-
-        val songIds = try {
-            JSONArray(JSONTokener(response)).iterator<JSONObject>().asSequence().toList().map {
-                o ->
-                o.getLong("songId")
-            }
-        } catch (e: JSONException) {
-            throw SearchException("Couldn't parse response: $e")
-        }
-        Log.d(TAG, "Server returned ${songIds.size} song(s)")
-
-        // Get the actual songs from SongDatabase.
-        // TODO: Using BUNDLE_CACHED doesn't work well in cases where the server returns a subset of
-        // all matching songs, since we may not have some of the subset locally. I'm not sure how to
-        // fix this without either making the server return all matching songs (yikes) or sending
-        // the cached songs to the server (also yikes).
-        return service.songDb.getSongs(
-            songIds,
-            onlyCached = intent.getBooleanExtra(BUNDLE_CACHED, false)
-        ).first
-    }
+    private suspend fun doNetworkSearch() = searchForSongsUsingNetwork(
+        service.songDb,
+        service.downloader,
+        artist = intent.getStringExtra(BUNDLE_ARTIST),
+        title = intent.getStringExtra(BUNDLE_TITLE),
+        album = intent.getStringExtra(BUNDLE_ALBUM),
+        shuffle = intent.getBooleanExtra(BUNDLE_SHUFFLE, false),
+        keywords = intent.getStringExtra(BUNDLE_KEYWORDS),
+        tags = intent.getStringExtra(BUNDLE_TAGS),
+        minRating = intent.getDoubleExtra(BUNDLE_MIN_RATING, -1.0),
+        maxPlays = intent.getIntExtra(BUNDLE_MAX_PLAYS, -1),
+        firstPlayed = intent.getIntExtra(BUNDLE_FIRST_PLAYED, 0),
+        lastPlayed = intent.getIntExtra(BUNDLE_LAST_PLAYED, 0)
+    )
 
     /** Update the activity to display [songs]. */
     private fun displaySongs() {

nup/src/main/java/MediaBrowserHelper.kt
@@ -6,10 +6,12 @@
 package org.erat.nup
 
 import android.content.res.Resources
+import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat.MediaItem
 import android.support.v4.media.MediaDescriptionCompat
 import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
+import androidx.media.utils.MediaConstants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,6 +20,7 @@ import kotlinx.coroutines.launch
 class MediaBrowserHelper(
     private val service: MediaBrowserServiceCompat,
     private val db: SongDatabase,
+    private val downloader: Downloader,
     private val networkHelper: NetworkHelper,
     private val scope: CoroutineScope,
     private val res: Resources,
@@ -35,6 +38,7 @@ class MediaBrowserHelper(
         }
 
         Log.d(TAG, "Notifying about changes for song database update")
+        service.notifyChildrenChanged(PRESETS_ID)
         service.notifyChildrenChanged(ARTISTS_ID)
         service.notifyChildrenChanged(ALBUMS_ID)
         service.notifyChildrenChanged(CACHED_ARTISTS_ID)
@@ -70,16 +74,30 @@ class MediaBrowserHelper(
                 // usually the case that the root can have up to 4 children, all of which can only
                 // be browsable.
                 val items = mutableListOf<MediaItem>()
+
+                val extras = Bundle()
+                extras.putInt(
+                    MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_PLAYABLE,
+                    MediaConstants.DESCRIPTION_EXTRAS_VALUE_CONTENT_STYLE_GRID_ITEM
+                )
+                items.add(makeMenuItem(res.getText(R.string.presets), PRESETS_ID, extras = extras))
+
                 if (networkHelper.isNetworkAvailable) {
-                    items.add(makeMenuItem(R.string.artists, ARTISTS_ID))
-                    items.add(makeMenuItem(R.string.albums, ALBUMS_ID))
+                    items.add(makeMenuItem(res.getText(R.string.artists), ARTISTS_ID))
+                    items.add(makeMenuItem(res.getText(R.string.albums), ALBUMS_ID))
                 } else {
                     // The full "Artists (cached)" string is too long for Android Auto's tabs.
-                    items.add(makeMenuItem(R.string.artists_star, CACHED_ARTISTS_ID))
-                    items.add(makeMenuItem(R.string.albums_star, CACHED_ALBUMS_ID))
+                    items.add(makeMenuItem(res.getText(R.string.artists_star), CACHED_ARTISTS_ID))
+                    items.add(makeMenuItem(res.getText(R.string.albums_star), CACHED_ALBUMS_ID))
                 }
-                items.add(makeMenuItem(R.string.shuffle, SHUFFLE_ID, playable = true))
                 result.sendResult(items)
+            }
+            parentId == PRESETS_ID -> {
+                result.sendResult(
+                    db.searchPresets.filter { it.play }.map { p ->
+                        makeMenuItem(p.name, PRESET_PREFIX + p.name, playable = true)
+                    }.toMutableList()
+                )
             }
             parentId == ARTISTS_ID -> {
                 val items = mutableListOf<MediaItem>()
@@ -151,13 +169,21 @@ class MediaBrowserHelper(
         }
     }
 
-    /** Create a [MediaItem] with the supplied string resource and media ID. */
-    private fun makeMenuItem(resId: Int, id: String, playable: Boolean = false): MediaItem {
-        val desc = MediaDescriptionCompat.Builder()
-            .setTitle(res.getText(resId))
+    /** Create a [MediaItem] with the supplied title and media ID. */
+    private fun makeMenuItem(
+        title: CharSequence,
+        id: String,
+        extras: Bundle? = null,
+        playable: Boolean = false,
+    ): MediaItem {
+        val builder = MediaDescriptionCompat.Builder()
+            .setTitle(title)
             .setMediaId(id)
-            .build()
-        return MediaItem(desc, if (playable) MediaItem.FLAG_PLAYABLE else MediaItem.FLAG_BROWSABLE)
+        if (extras != null) builder.setExtras(extras)
+        return MediaItem(
+            builder.build(),
+            if (playable) MediaItem.FLAG_PLAYABLE else MediaItem.FLAG_BROWSABLE
+        )
     }
 
     /** Create a [MediaItem] for the artist described by [row]. */
@@ -205,8 +231,8 @@ class MediaBrowserHelper(
     /** Query the database for songs identified by [id]. */
     public suspend fun getSongsForMediaId(id: String): List<Song> {
         return when {
-            id == SHUFFLE_ID ->
-                searchForSongs(db, "", online = networkHelper.isNetworkAvailable)
+            id.startsWith(PRESET_PREFIX) ->
+                doPresetSearch(id.substring(PRESET_PREFIX.length))
             id.startsWith(ALBUM_ID_PREFIX) ->
                 db.query(albumId = id.substring(ALBUM_ID_PREFIX.length))
             id.startsWith(CACHED_ALBUM_ID_PREFIX) ->
@@ -220,17 +246,37 @@ class MediaBrowserHelper(
         }
     }
 
+    private suspend fun doPresetSearch(name: String): List<Song> {
+        val p = db.searchPresets.find { it.name == name }
+        if (p == null) {
+            Log.e(TAG, "Unknown search preset \"$name\"")
+            return listOf<Song>()
+        }
+        return searchForSongsUsingNetwork(
+            db,
+            downloader,
+            tags = if (p.tags != "") p.tags else null,
+            minRating = p.minRating,
+            unrated = p.unrated,
+            firstPlayed = p.firstPlayed,
+            lastPlayed = p.lastPlayed,
+            firstTrack = p.firstTrack,
+            shuffle = p.shuffle
+        )
+    }
+
     companion object {
         private const val TAG = "MediaBrowserHelper"
 
         public const val ROOT_ID = "root_id"
 
+        private const val PRESETS_ID = "presets"
         private const val ARTISTS_ID = "artists"
         private const val ALBUMS_ID = "albums"
         private const val CACHED_ARTISTS_ID = "cached_artists"
         private const val CACHED_ALBUMS_ID = "cached_albums"
-        private const val SHUFFLE_ID = "shuffle"
 
+        private const val PRESET_PREFIX = "preset_"
         private const val ARTIST_ID_PREFIX = "artist_"
         private const val ALBUM_ID_PREFIX = "album_"
         private const val CACHED_ARTIST_ID_PREFIX = "cached_artist_"

nup/src/main/java/NupService.kt
@@ -402,7 +402,9 @@ class NupService :
         )
         sessionToken = mediaSessionManager.session.sessionToken
 
-        mediaBrowserHelper = MediaBrowserHelper(this, songDb, networkHelper, scope, getResources())
+        mediaBrowserHelper = MediaBrowserHelper(
+            this, songDb, downloader, networkHelper, scope, getResources()
+        )
 
         notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationCreator = NotificationCreator(

nup/src/main/java/Search.kt
@@ -6,6 +6,12 @@
 package org.erat.nup
 
 import android.util.Log
+import java.net.URLEncoder
+import java.util.Date
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import org.json.JSONTokener
 
 // https://developer.android.com/guide/topics/media-apps/interacting-with-assistant
 //
@@ -148,6 +154,72 @@ suspend fun searchForSongs(
     // Fall back to a substring album match.
     return db.query(album = query, substring = true)
 }
+
+@Throws(SearchException::class)
+suspend fun searchForSongsUsingNetwork(
+    db: SongDatabase,
+    downloader: Downloader,
+    artist: String? = null,
+    title: String? = null,
+    album: String? = null,
+    shuffle: Boolean = false,
+    keywords: String? = null,
+    tags: String? = null,
+    minRating: Double = -1.0,
+    unrated: Boolean = false,
+    maxPlays: Int = -1,
+    firstPlayed: Int = 0,
+    lastPlayed: Int = 0,
+    firstTrack: Boolean = false,
+    onlyCached: Boolean = false,
+): List<Song> {
+    var params = ""
+    val add = fun(k: String, v: String?) {
+        if (v.isNullOrEmpty()) return
+        if (!params.isEmpty()) params += "&"
+        params += "$k=${URLEncoder.encode(v, "utf-8")}"
+    }
+
+    add("artist", artist)
+    add("title", title)
+    add("album", album)
+    add("shuffle", if (shuffle) "1" else "")
+    add("keywords", keywords)
+    add("tags", tags)
+    add("minRating", if (minRating >= 0) "%.2f".format(minRating) else "")
+    add("unrated", if (unrated) "1" else "")
+    add("maxPlays", if (maxPlays >= 0) maxPlays.toString() else "")
+    add("firstTrack", if (firstTrack) "1" else "")
+
+    // We get numbers of seconds before the current time, but the server
+    // wants timestamps as seconds since the epoch.
+    val now = Date().getTime() / 1000
+    if (firstPlayed > 0) add("minFirstPlayed", (now - firstPlayed).toString())
+    if (lastPlayed > 0) add("maxLastPlayed", (now - lastPlayed).toString())
+
+    val (response, error) = downloader.downloadString("/query?$params")
+    response ?: throw SearchException(error!!)
+
+    val songIds = try {
+        JSONArray(JSONTokener(response)).iterator<JSONObject>().asSequence().toList().map {
+            o ->
+            o.getLong("songId")
+        }
+    } catch (e: JSONException) {
+        throw SearchException("Couldn't parse response: $e")
+    }
+    Log.d(TAG, "Server returned ${songIds.size} song(s)")
+
+    // Get the actual songs from SongDatabase.
+    // TODO: Using onlyCached doesn't work well in cases where the server returns a subset of all
+    // matching songs, since we may not have some of the subset locally. I'm not sure how to fix
+    // this without either making the server return all matching songs (yikes) or sending the cached
+    // songs to the server (also yikes).
+    return db.getSongs(songIds, onlyCached = onlyCached).first
+}
+
+/** Thrown if an error is encountered while searching. */
+class SearchException(reason: String) : Exception(reason)
 
 private fun findArtistExact(rows: List<StatsRow>, artist: String) =
     rows.find { it.key.artist.equals(artist, ignoreCase = true) }

nup/src/main/java/SettingsActivity.kt
@@ -250,6 +250,8 @@ private fun getSyncSummary(
         }
         SongDatabase.SyncState.STARTING ->
             res.getString(R.string.sync_progress_starting)
+        SongDatabase.SyncState.UPDATING_PRESETS ->
+            res.getString(R.string.sync_progress_updating_presets)
         SongDatabase.SyncState.UPDATING_SONGS ->
             res.getQuantityString(R.plurals.sync_update_fmt, updatedSongs, updatedSongs)
         SongDatabase.SyncState.DELETING_SONGS ->

commit 5a0719cea586bdf690a0859110975355c6e925c6

Fix 'cached' checkbox for network search.
Support for pruning the server's results based on each
song's cached state was still there, but doServerSearch()
wasn't passing the checkbox's value to searchServer().
I think that this was broken by 85b9593b1c89.

nup/src/main/java/SearchResultsActivity.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import org.erat.nup.NupActivity.Companion.service
 
-/** Displays a list of songs from a search result. */
+/** Performs a search and displays the resulting songs. */
 class SearchResultsActivity : AppCompatActivity() {
     private var songs = listOf<Song>()
 
@@ -177,7 +177,8 @@ class SearchResultsActivity : AppCompatActivity() {
         orderByLastPlayed = intent.getBooleanExtra(BUNDLE_ORDER_BY_LAST_PLAYED, false),
         maxPlays = intent.getIntExtra(BUNDLE_MAX_PLAYS, -1),
         firstPlayed = intent.getIntExtra(BUNDLE_FIRST_PLAYED, 0),
-        lastPlayed = intent.getIntExtra(BUNDLE_LAST_PLAYED, 0)
+        lastPlayed = intent.getIntExtra(BUNDLE_LAST_PLAYED, 0),
+        onlyCached = intent.getBooleanExtra(BUNDLE_CACHED, false),
     )
 
     /** Update the activity to display [songs]. */