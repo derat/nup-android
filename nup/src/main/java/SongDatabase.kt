derat/nup-android

commit 8024a484eadf2cfa2312aa6bdb30e25fc5b60483

Fetch user info from server.
Make SongDatabase fetch /user from the server and filter
songs that contain excluded tags. Also report the user's
guest-mode state to listeners so that NupService can update
the preference accordingly. See #50.

nup/src/main/java/SongDatabase.kt
@@ -68,6 +68,7 @@ class SongDatabase(
     enum class SyncState {
         IDLE,
         STARTING,
+        FETCHING_USER_INFO,
         UPDATING_PRESETS,
         UPDATING_SONGS,
         DELETING_SONGS,
@@ -78,6 +79,8 @@ class SongDatabase(
     interface Listener {
         /** Called when [syncState] changes. */
         fun onSyncChange(state: SyncState, updatedSongs: Int)
+        /** Called when [userInfo] has been fetched from the server. */
+        fun onUserInfoFetch(userInfo: UserInfo)
         /** Called synchronization finishes (either successfully or not). */
         fun onSyncDone(success: Boolean, message: String)
         /** Called when aggregate stats have been updated. */
@@ -308,7 +311,7 @@ class SongDatabase(
     suspend fun handleSongEvicted(songId: Long) {
         opener.getDb() task@{ db ->
             if (db == null) return@task // quit() already called
-            db.delete("CachedSongs ", "SongId = ?", arrayOf(songId.toString()))
+            db.delete("CachedSongs", "SongId = ?", arrayOf(songId.toString()))
         }
     }
 
@@ -367,6 +370,11 @@ class SongDatabase(
                 notifySyncDone(false, context.getString(R.string.network_is_unavailable))
                 return
             }
+
+            setSyncState(SyncState.FETCHING_USER_INFO, 0)
+            val userInfo = fetchUserInfo()
+            listenerExecutor.execute { listener.onUserInfoFetch(userInfo) }
+
             opener.getDb() task@{ db ->
                 if (db == null) {
                     notifySyncDone(false, "Database already closed")
@@ -380,7 +388,7 @@ class SongDatabase(
                 }
                 loadSearchPresets(db)
 
-                val songsRes = syncSongs(db)
+                val songsRes = syncSongs(db, userInfo.excludedTags)
                 if (!songsRes.success) {
                     notifySyncDone(false, songsRes.error!!)
                     return@task
@@ -397,7 +405,7 @@ class SongDatabase(
     data class SyncResult(val success: Boolean, val updatedItems: Int, val error: String?)
 
     /** Update local list of songs on behalf of [syncWithServer]. */
-    private fun syncSongs(db: SQLiteDatabase): SyncResult {
+    private fun syncSongs(db: SQLiteDatabase, excludedTags: Set<String>): SyncResult {
         var updatedSongs = 0
 
         if (db.inTransaction()) throw RuntimeException("Already in transaction")
@@ -417,12 +425,15 @@ class SongDatabase(
             }
 
             try {
-                updatedSongs += syncSongUpdates(db, prevStartNs, false)
-                updatedSongs += syncSongUpdates(db, prevStartNs, true)
+                updatedSongs += syncSongUpdates(db, prevStartNs, false /* deleted */, excludedTags)
+                updatedSongs += syncSongUpdates(db, prevStartNs, true /* deleted */, excludedTags)
             } catch (e: ServerException) {
                 Log.e(TAG, e.message!!)
                 return SyncResult(false, updatedSongs, e.message)
             }
+
+            // Delete any existing songs that have now-excluded tags.
+            updatedSongs += deleteSongsWithTags(db, excludedTags)
 
             val values = ContentValues(2)
             values.put("LocalTimeNsec", Date().time * 1000 * 1000)
@@ -455,6 +466,27 @@ class SongDatabase(
     /** Thrown by [syncSongUpdates] and [syncSearchPresets] if an error is encountered. */
     class ServerException(reason: String) : Exception(reason)
 
+    /** User information from the server's /user endpoint. */
+    data class UserInfo(
+        val guest: Boolean = false,
+        val excludedTags: Set<String> = setOf<String>(),
+    )
+
+    @Throws(ServerException::class)
+    private fun fetchUserInfo(): UserInfo {
+        val (response, error) = downloader.downloadString("/user")
+        response ?: throw ServerException(error!!)
+        try {
+            val obj = JSONObject(JSONTokener(response))
+            return UserInfo(
+                guest = obj.optBoolean("guest"),
+                excludedTags = jsonStringArrayToList(obj.optJSONArray("excludedTags")).toSet(),
+            )
+        } catch (e: JSONException) {
+            throw ServerException("Couldn't parse response: $e")
+        }
+    }
+
     /**
      * Get updated songs from the server on behalf of [syncSongs].
      *
@@ -465,6 +497,7 @@ class SongDatabase(
         db: SQLiteDatabase,
         prevStartTimeNsec: Long,
         deleted: Boolean,
+        excludedTags: Set<String>,
     ): Int {
         // The server breaks its results up into batches instead of sending us a bunch of songs
         // at once, so use the cursor that it returns to start in the correct place in the next
@@ -522,15 +555,15 @@ class SongDatabase(
                                     }
                                 }
 
-                                // JSONArray unfortunately doesn't expose an iterator.
                                 var tags = ""
                                 val tagsArray = item.optJSONArray("tags")
                                 if (tagsArray != null && tagsArray.length() > 0) {
-                                    val tagsList = arrayListOf<String>()
-                                    for (i in 0 until tagsArray.length()) {
-                                        tagsList.add(tagsArray.optString(i))
+                                    val list = jsonStringArrayToList(tagsArray)
+                                    if (list.any { excludedTags.contains(it) }) {
+                                        Log.d(TAG, "Skipping song $songId due to excluded tag")
+                                        continue
                                     }
-                                    tags = "|" + tagsList.joinToString("|") + "|"
+                                    tags = "|" + list.joinToString("|") + "|"
                                 }
 
                                 val values = ContentValues(18)
@@ -580,6 +613,22 @@ class SongDatabase(
             numUpdates,
         )
         return numUpdates
+    }
+
+    /** Delete rows from the Songs table with one or more of [tags]. */
+    private fun deleteSongsWithTags(db: SQLiteDatabase, tags: Set<String>): Int {
+        if (tags.isEmpty()) return 0
+
+        val clauses = mutableListOf<String>()
+        val params = mutableListOf<String>()
+        tags.forEach {
+            val tag = it.replace("%", " %") // escape '%' characters; see [query]
+            clauses.add("Tags LIKE ? ESCAPE ' '")
+            params.add("%|$tag|%")
+        }
+        val deleted = db.delete("Songs", clauses.joinToString(" OR "), params.toTypedArray())
+        Log.d(TAG, "Deleted $deleted existing song(s) due to excluded tags")
+        return deleted
     }
 
     /** Fetch and save all search presets from the server. */
@@ -977,4 +1026,16 @@ fun normalizeForSearch(s: String): String {
         .replace("\\p{Mn}".toRegex(), "")
         .trim()
         .lowercase()
+}
+
+/** Convert an optional array of strings into a list. */
+fun jsonStringArrayToList(array: JSONArray?): List<String> {
+    var list = arrayListOf<String>()
+    if (array != null) {
+        // JSONArray unfortunately doesn't expose an iterator.
+        for (i in 0 until array.length()) {
+            list.add(array.optString(i))
+        }
+    }
+    return list
 }

nup/src/main/java/NupService.kt
@@ -1107,6 +1107,14 @@ class NupService :
         }
     }
 
+    override fun onUserInfoFetch(userInfo: SongDatabase.UserInfo) {
+        assertOnMainThread()
+        Log.d(TAG, "Fetched user info: $userInfo")
+        scope.launch(Dispatchers.IO) {
+            prefs.edit().putBoolean(NupPreferences.GUEST_MODE, userInfo.guest).commit()
+        }
+    }
+
     override fun onSyncDone(success: Boolean, message: String) {
         assertOnMainThread()
         Log.d(TAG, "Sync ${if (success) "succeeded" else "failed"}: $message")

nup/src/main/java/SettingsActivity.kt
@@ -145,6 +145,9 @@ class SettingsFragment :
 
     override fun onSongDatabaseUpdate() {
         findPreference<YesNoPreference>(NupPreferences.SYNC_SONG_LIST)!!.notifyChanged()
+        // TODO: If syncing updated NupPreferences.GUEST_MODE, the corresponding CheckBoxPreference
+        // is stale at this point. Its notifyChanged() member is private, so we can't call it.
+        // I'm not sure how to fix this; it has the right state after this activity is recreated.
     }
     override fun onCacheSizeChange() {
         findPreference<YesNoPreference>(NupPreferences.CLEAR_CACHE)!!.notifyChanged()
@@ -268,6 +271,8 @@ private fun getSyncSummary(
         }
         SongDatabase.SyncState.STARTING ->
             res.getString(R.string.sync_progress_starting)
+        SongDatabase.SyncState.FETCHING_USER_INFO ->
+            res.getString(R.string.sync_progress_fetching_user_info)
         SongDatabase.SyncState.UPDATING_PRESETS ->
             res.getString(R.string.sync_progress_updating_presets)
         SongDatabase.SyncState.UPDATING_SONGS ->

nup/src/main/res/values/strings.xml
@@ -95,6 +95,7 @@
   <string name="startup_message_text">Feed me some songs.</string>
   <string name="sync_already">Already synchronizing</string>
   <string name="sync_complete">Synchronization complete</string>
+  <string name="sync_progress_fetching_user_info">Fetching user info...</string>
   <string name="sync_progress_rebuilding_stats_tables">Rebuilding stats tables...</string>
   <string name="sync_progress_starting">Starting sync...</string>
   <string name="sync_progress_updating_presets">Updating presets...</string>

nup/src/test/java/SongDatabaseTest.kt
@@ -70,6 +70,9 @@ class SongDatabaseTest {
                 else -> {}
             }
         }
+        override fun onUserInfoFetch(userInfo: SongDatabase.UserInfo) {
+            lastSyncUserInfo = userInfo
+        }
         override fun onSyncDone(success: Boolean, message: String) {
             lastSyncSuccess = success
             lastSyncMessage = message
@@ -80,6 +83,7 @@ class SongDatabaseTest {
     var lastSyncDeletedSongs = 0
     var lastSyncSuccess = false
     var lastSyncMessage = ""
+    var lastSyncUserInfo: SongDatabase.UserInfo? = null
     var aggregateDataUpdates = 0
 
     // Server info returned by [downloader].
@@ -88,6 +92,7 @@ class SongDatabaseTest {
     val serverSongs = mutableListOf<SongInfo>()
     val serverDelSongs = mutableListOf<SongInfo>()
     val serverSearchPresets = mutableListOf<SearchPreset>()
+    var serverUserInfo = SongDatabase.UserInfo()
 
     // Matches HTTP request paths from [SongDatabase.syncSongUpdates].
     val exportRegex = Regex(
@@ -117,6 +122,12 @@ class SongDatabaseTest {
                                 .toString(),
                             null
                         )
+                        (path == "/user") -> {
+                            val obj = JSONObject()
+                            obj.put("guest", serverUserInfo.guest)
+                            obj.put("excludedTags", JSONArray(serverUserInfo.excludedTags))
+                            return Downloader.DownloadStringResult(obj.toString(), null)
+                        }
                         exportRegex.matches(path) -> {
                             val match = exportRegex.find(path)!!
                             val lastMod = match.groupValues[1].toLong()
@@ -137,6 +148,7 @@ class SongDatabaseTest {
         lastSyncUpdatedSongs = 0
         lastSyncDeletedSongs = 0
         lastSyncSuccess = false
+        lastSyncUserInfo = null
         aggregateDataUpdates = 0
 
         // Reset server data.
@@ -144,6 +156,7 @@ class SongDatabaseTest {
         serverSongs.clear()
         serverDelSongs.clear()
         serverSearchPresets.clear()
+        serverUserInfo = SongDatabase.UserInfo()
 
         initDb()
     }
@@ -178,6 +191,7 @@ class SongDatabaseTest {
             tags = listOf("instrumental", "electronic", "10%"),
         )
         serverSongs.addAll(listOf(SongInfo(s1), SongInfo(s2), SongInfo(s3), SongInfo(s4)))
+        serverUserInfo = SongDatabase.UserInfo(guest = true, excludedTags = setOf("foo", "bar"))
 
         db.syncWithServer()
         assertEquals(SongDatabase.SyncState.IDLE, db.syncState)
@@ -185,6 +199,7 @@ class SongDatabaseTest {
         assertEquals(4, lastSyncUpdatedSongs)
         assertTrue(db.aggregateDataLoaded)
         assertEquals(4, db.numSongs)
+        assertEquals(serverUserInfo, lastSyncUserInfo)
 
         assertEquals(listOf(s1, s2, s3, s4), db.query())
         assertEquals(listOf(s1, s2), db.query(artist = "Ä"))
@@ -257,6 +272,20 @@ class SongDatabaseTest {
         assertEquals(1, lastSyncDeletedSongs)
         assertEquals(2, db.numSongs)
         assertEquals(listOf(s1u, s3), db.query())
+
+        // At time 2, add fourth and fifth songs and introduce an excluded tag that excludes
+        // the fourth song and drops the updated first song.
+        serverNowNs++
+        val s4 = makeSong("A", "Track 4", "Album 1", 4, rating = 3, tags = listOf("rock", "drums"))
+        val s5 = makeSong("A", "Track 5", "Album 1", 5, rating = 4, tags = listOf("jazz"))
+        serverSongs.add(SongInfo(s4, serverNowNs))
+        serverSongs.add(SongInfo(s5, serverNowNs))
+        serverUserInfo = SongDatabase.UserInfo(excludedTags = setOf("rock"))
+        db.syncWithServer()
+        assertTrue(lastSyncMessage, lastSyncSuccess)
+        assertEquals(2, lastSyncUpdatedSongs) // new song and dropped song
+        assertEquals(2, db.numSongs)
+        assertEquals(listOf(s3, s5), db.query())
     }
 
     @Test fun aggregateData() = runTest(dispatcher) {

commit 8278b6fa078eec496e3878c425674601eb3eba15

Fix uncaught user info exception in SongDatabase.
Fix a bug introduced by 8024a484eadf where an uncaught
exception would crash the app if fetching user info failed.

nup/src/main/java/SongDatabase.kt
@@ -372,8 +372,12 @@ class SongDatabase(
             }
 
             setSyncState(SyncState.FETCHING_USER_INFO, 0)
-            val userInfo = fetchUserInfo()
-            listenerExecutor.execute { listener.onUserInfoFetch(userInfo) }
+            val (userInfo, userError) = fetchUserInfo()
+            if (userError != null) {
+                notifySyncDone(false, userError)
+                return
+            }
+            listenerExecutor.execute { listener.onUserInfoFetch(userInfo!!) }
 
             opener.getDb() task@{ db ->
                 if (db == null) {
@@ -388,7 +392,7 @@ class SongDatabase(
                 }
                 loadSearchPresets(db)
 
-                val songsRes = syncSongs(db, userInfo.excludedTags)
+                val songsRes = syncSongs(db, userInfo!!.excludedTags)
                 if (!songsRes.success) {
                     notifySyncDone(false, songsRes.error!!)
                     return@task
@@ -398,6 +402,33 @@ class SongDatabase(
             }
         } finally {
             setSyncState(SyncState.IDLE, 0)
+        }
+    }
+
+    /** User information from the server's /user endpoint. */
+    data class UserInfo(
+        val guest: Boolean = false,
+        val excludedTags: Set<String> = setOf<String>(),
+    )
+
+    /** Returned by [fetchUserInfo]. */
+    private data class FetchUserInfoResult(val info: UserInfo?, val error: String?)
+
+    /** Fetch info about the current user from the server. */
+    private fun fetchUserInfo(): FetchUserInfoResult {
+        val (response, error) = downloader.downloadString("/user")
+        response ?: return FetchUserInfoResult(null, error!!)
+        try {
+            val obj = JSONObject(JSONTokener(response))
+            return FetchUserInfoResult(
+                UserInfo(
+                    guest = obj.optBoolean("guest"),
+                    excludedTags = jsonStringArrayToList(obj.optJSONArray("excludedTags")).toSet(),
+                ),
+                null
+            )
+        } catch (e: JSONException) {
+            return FetchUserInfoResult(null, "Couldn't parse response: $e")
         }
     }
 
@@ -465,27 +496,6 @@ class SongDatabase(
 
     /** Thrown by [syncSongUpdates] and [syncSearchPresets] if an error is encountered. */
     class ServerException(reason: String) : Exception(reason)
-
-    /** User information from the server's /user endpoint. */
-    data class UserInfo(
-        val guest: Boolean = false,
-        val excludedTags: Set<String> = setOf<String>(),
-    )
-
-    @Throws(ServerException::class)
-    private fun fetchUserInfo(): UserInfo {
-        val (response, error) = downloader.downloadString("/user")
-        response ?: throw ServerException(error!!)
-        try {
-            val obj = JSONObject(JSONTokener(response))
-            return UserInfo(
-                guest = obj.optBoolean("guest"),
-                excludedTags = jsonStringArrayToList(obj.optJSONArray("excludedTags")).toSet(),
-            )
-        } catch (e: JSONException) {
-            throw ServerException("Couldn't parse response: $e")
-        }
-    }
 
     /**
      * Get updated songs from the server on behalf of [syncSongs].